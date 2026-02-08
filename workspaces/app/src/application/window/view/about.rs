ade2ea55e9ea8b08d94120bc3eabcdddc9a3469a
Pim Vermeer <pim.vermeer@gmail.com>
2026-02-08 12:57:40 +0000

    refactor(about): revert translation of about
    AboutDialog is already translated by libAdwaita

--- workspaces/app/src/application/window/view/about.rs
+++ workspaces/app/src/application/window/view/about.rs
@@ -1,16 +1,6 @@
 use common::{assets, config, utils::OnceLockExt};
-use gtk::{
-    License,
-    glib::{
-        GString,
-        object::{Cast, ObjectExt},
-    },
-    prelude::{BuildableExt, WidgetExt},
-};
-use libadwaita::{
-    AboutDialog, ActionRow,
-    prelude::{AdwDialogExt, PreferencesRowExt},
-};
+use gtk::License;
+use libadwaita::AboutDialog;
 use semver::Version;
 use serde::{Deserialize, Serialize};
 use std::fmt::Write as _;
@@ -23,87 +13,6 @@ pub struct CreditsYaml {
     name: String,
     link: Option<String>,
 }
-/// Downcast and translate nested ``AboutDialog`` widgets
-enum AboutDialogWidget {
-    Changelog(ActionRow),
-    Details(ActionRow),
-    Website(ActionRow),
-    Support(ActionRow),
-    Issue(ActionRow),
-    Troubleshooting(ActionRow),
-    Credits(ActionRow),
-    Legal(ActionRow),
-    Acknowledgements(ActionRow),
-}
-impl AboutDialogWidget {
-    fn from_widget(widget: &gtk::Widget) -> Option<Self> {
-        let id: &str = &widget.buildable_id().unwrap_or_default();
-        let title: &str = if widget.has_property("title") {
-            &widget.property::<GString>("title")
-        } else {
-            ""
-        };
-
-        match (id, title) {
-            ("whats_new_row", _) | (_, "What’s _New") => {
-                widget.downcast_ref().cloned().map(Self::Changelog)
-            }
-            ("details_row", _) | (_, "_Details") => {
-                widget.downcast_ref().cloned().map(Self::Details)
-            }
-            ("website_row", _) | (_, "_Website") => {
-                widget.downcast_ref().cloned().map(Self::Website)
-            }
-            ("support_row", _) | (_, "_Support Questions") => {
-                widget.downcast_ref().cloned().map(Self::Support)
-            }
-            ("issue_row", _) | (_, "_Report an Issue") => {
-                widget.downcast_ref().cloned().map(Self::Issue)
-            }
-            ("troubleshooting_row", _) | (_, "_Troubleshooting") => {
-                widget.downcast_ref().cloned().map(Self::Troubleshooting)
-            }
-            // These suddenly don't have a buildable_id in inspector
-            (_, "_Credits") => widget.downcast_ref().cloned().map(Self::Credits),
-            (_, "_Legal") => widget.downcast_ref().cloned().map(Self::Legal),
-            (_, "Acknowledgements") => widget.downcast_ref().cloned().map(Self::Acknowledgements),
-
-            _ => None,
-        }
-    }
-
-    fn translate_entry(&self) {
-        match self {
-            Self::Changelog(action_row) => {
-                action_row.set_title(&t!("about.changelog"));
-            }
-            Self::Details(action_row) => {
-                action_row.set_title(&t!("about.details"));
-            }
-            Self::Website(action_row) => {
-                action_row.set_title(&t!("about.website"));
-            }
-            Self::Support(action_row) => {
-                action_row.set_title(&t!("about.support"));
-            }
-            Self::Issue(action_row) => {
-                action_row.set_title(&t!("about.issue"));
-            }
-            Self::Troubleshooting(action_row) => {
-                action_row.set_title(&t!("about.troubleshooting"));
-            }
-            Self::Credits(action_row) => {
-                action_row.set_title(&t!("about.credits"));
-            }
-            Self::Legal(action_row) => {
-                action_row.set_title(&t!("about.legal"));
-            }
-            Self::Acknowledgements(action_row) => {
-                action_row.set_title(&t!("about.acknowledgements"));
-            }
-        }
-    }
-}
 
 pub fn get_dialog() -> AboutDialog {
     let license = match config::LICENSE.get_value().as_str() {
@@ -112,7 +21,7 @@ pub fn get_dialog() -> AboutDialog {
         _ => panic!("Could not convert license"),
     };
 
-    let about_dialog = AboutDialog::builder()
+    AboutDialog::builder()
         .application_icon(config::APP_ID.get_value())
         .application_name(config::APP_NAME.get_value())
         .version(config::VERSION.get_value())
@@ -123,11 +32,7 @@ pub fn get_dialog() -> AboutDialog {
         .copyright(format!("© 2025 {}", config::DEVELOPER.get_value()))
         .documenters(parse_documenters())
         .translator_credits(parse_translators())
-        .build();
-
-    translate_about_dialog_widgets(&about_dialog);
-
-    about_dialog
+        .build()
 }
 
 fn parse_release_notes_xml() -> String {
@@ -188,31 +93,6 @@ fn parse_release_notes_xml() -> String {
     }
 
     release_xml
-}
-
-fn translate_about_dialog_widgets(about_dialog: &AboutDialog) {
-    /// Recursive fn to translate child nested child widgets
-    fn recursive_translate<F>(widget: &gtk::Widget, recursive_fn: &F)
-    where
-        F: Fn(&gtk::Widget),
-    {
-        recursive_fn(widget);
-
-        let mut child = widget.first_child();
-        while let Some(w) = child {
-            recursive_translate(&w, recursive_fn);
-            child = w.next_sibling();
-        }
-    }
-
-    let translate_widget = &|widget: &gtk::Widget| {
-        let Some(found_widget) = AboutDialogWidget::from_widget(widget) else {
-            return;
-        };
-        found_widget.translate_entry();
-    };
-
-    recursive_translate(&about_dialog.child().unwrap(), translate_widget);
 }
 
 fn parse_documenters() -> Vec<String> {
